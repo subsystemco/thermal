1a52ebb359be8394708bc19998579045e732bf12
Greg Leppert <greg@leppert.me>
2016-10-08 21:44:36 -0400

    Add IAP helper

--- src/secret_shopper/apple.clj
+++ src/secret_shopper/apple.clj
@@ -19,11 +19,28 @@
 
 (defn dates
   "Return a map of dates in Apple's three formats: timestamp, GMT, and PST."
-  [name, date]
-  (let [gmt-tz-id "Etc/GMT"
+  [prop-name date]
+  (let [prop-name (name prop-name)
+        gmt-tz-id "Etc/GMT"
         gmt-tz (t/time-zone-for-id gmt-tz-id)
         la-tz-id "America/Los_Angeles"
         la-tz (t/time-zone-for-id la-tz-id)]
-    {(keyword (str name "_date")) (str (format-date (t/to-time-zone date gmt-tz)) " " gmt-tz-id)
-     (keyword (str name "_date_ms")) (str (c/to-epoch date))
-     (keyword (str name "_date_pst")) (str (format-date (t/to-time-zone date la-tz)) " " la-tz-id)}))
+    {(keyword (str prop-name "_date")) (str (format-date (t/to-time-zone date gmt-tz)) " " gmt-tz-id)
+     (keyword (str prop-name "_date_ms")) (str (c/to-epoch date))
+     (keyword (str prop-name "_date_pst")) (str (format-date (t/to-time-zone date la-tz)) " " la-tz-id)}))
+
+(defn iap
+  "Generate an In App Purchase receipt."
+  ([product duration date org-date]
+   (iap product duration date org-date false))
+  ([product duration date org-date trial]
+   (merge
+     {:quantity "1"
+      :product_id product
+      :transaction_id (transaction-id date)
+      :original_transaction_id (transaction-id org-date)
+      :web_order_line_item_id (web-order-line-item-id date)
+      :is_trial_period (str trial)}
+     (dates :purchase date)
+     (dates :original_purchase org-date)
+     (dates :expires (t/plus date duration)))))
