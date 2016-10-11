e3bc1541c150f2592bde7e5fa82a856f07d4ff3b
Greg Leppert <greg@leppert.me>
2016-10-11 07:11:17 -0400

    Add response output

--- src/thermal/apple.clj
+++ src/thermal/apple.clj
@@ -1,9 +1,19 @@
 (ns thermal.apple
   (:require [clj-time.core :as t]
             [clj-time.coerce :as c]
-            [clj-time.local :as l]))
+            [clj-time.local :as l]
+            [clj-time.periodic :as p]
+            [clojure.data.codec.base64 :as b64]
+            [clojure.data.json :as json]))
 
-(def BUNDLE_ID "test-bundle-id")
+(def ENVIRONMENT "Sandbox")
+(def BUNDLE-ID "test-bundle-id")
+(def PRODUCT-ID "test-product-id")
+(def ORIGINAL-PURCHASE-DATE (t/from-time-zone (t/date-time 2013 8 1 7) gmt-tz))
+
+(defn string->base64 [input]
+  "Encodes an UTF-8 string into a base64 UTF-8 string"
+  (String. (b64/encode (.getBytes input)) "UTF-8"))
 
 (defn transaction-id
   "Generate a transaction id from a date."
@@ -55,13 +65,51 @@
     {:receipt_type "ProductionSandbox"
      :adam_id 0
      :app_item_id 0
-     :bundle_id BUNDLE_ID
+     :bundle_id BUNDLE-ID
      :application_version "12345"
      :download_id 0
      :version_external_identifier 0
      :original_application_version "1.0"
      :in_app iaps}
-    ;; original_purchase is always the same date
-    (dates :original_purchase (t/from-time-zone (t/date-time 2013 8 1 7) gmt-tz))
+    (dates :original_purchase ORIGINAL-PURCHASE-DATE)
     (dates :receipt_creation date)
     (dates :request (t/now))))
+
+(defn response
+  "Generate an Apple receipt validation response."
+  [& subs]
+  (let [now (t/now)
+        defaults {:product PRODUCT-ID
+                  :canceled false
+                  :trialed false
+                  :plan_duration (t/months 1)
+                  :start_date now}
+        subs (->> subs
+               (sort-by :start_date)
+               (partition 2 1)
+               (#(concat % (list (list (last subs) {}))))
+               (map
+                 #(merge (assoc defaults :end_date (:start_date (last %) now)) (first %))))
+        iaps (->> subs
+               (mapcat (fn [sub]
+                         (for [date (p/periodic-seq (:start_date sub) (:plan_duration sub))
+                               :while (or (t/before? date (:end_date sub))
+                                        (t/equal? date (:end_date sub)))]
+                           (iap
+                             (:product sub)
+                             (:plan_duration sub)
+                             date
+                             (:start_date sub)
+                             (and (:trialed sub) (= date (:start_date sub))))))))]
+
+    {:status 0
+     :environment ENVIRONMENT
+     :receipt (receipt (-> subs first :start_date) iaps)
+     :latest_receipt_info iaps
+     :latest_receipt (string->base64 (json/write-str iaps))}))
+
+(defn scratch
+  []
+  (response {:product "com.electricobjects.artclub.subscription.monthly.notrial"
+             :plan_duration (t/months 1)
+             :start_date (t/date-time 2016 8 14 4 3 27 456)}))
